a1225f6d9b720f36e8c054c8b6e8aee4a1512ac5
Gurkan Uzunca <selam@gurkanuzunca.com>
2016-07-30 16:08:38 +0300

    uploadPath fakeimage eklemeleri yapıldı. lang fonksiyonu yazıldı.

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -96,9 +96,18 @@ function makeSlug($str)
 }
 
 
-function uploadPath($file, $path = '')
+function uploadPath($file, $path = '', $width = 480, $height = null, $text = '?')
 {
-    return 'public/upload/'. (empty($path) ?: "$path/") . $file;
+    $fullpath = 'public/upload/'. (empty($path) ?: "$path/") . $file;
+    if (is_file($fullpath)) {
+        return $fullpath;
+    }
+
+    if (empty($height)) {
+        $height = $width;
+    }
+
+    return 'http://fakeimg.pl/'. $width .'x'. $height .'/?text='. $text;
 }
 
 
@@ -123,4 +132,50 @@ function money($number, $fractional = false)
         }
     }
     return $number;
+}
+
+
+
+function lang($line, $convert = null)
+{
+    $slug = (! is_null($convert) ? $convert.'-':'') . makeSlug($line);
+    $trans = get_instance()->lang->line($slug);
+
+    if (empty($trans)) {
+        $trans = (! is_null($convert) ? strConvert($line, $convert, get_instance()->language) : $line);
+        $filepath = APPPATH .'/language/'. get_instance()->language .'/site_lang.php';
+        $file = fopen($filepath, FOPEN_WRITE_CREATE);
+        $data = '$lang[\''. $slug .'\'] = \''. $trans .'\';'. PHP_EOL;
+
+        flock($file, LOCK_EX);
+        fwrite($file, $data);
+        flock($file, LOCK_UN);
+        fclose($file);
+
+        return $trans;
+    }
+
+    return $trans;
+}
+
+
+function strConvert($string, $to = null, $lang = 'tr')
+{
+    switch ($to) {
+        case 'upper':
+            $string = mb_strtoupper($lang === 'tr' ? str_replace('i', 'İ', $string) : $string, 'UTF-8');
+            break;
+        case 'lower':
+            $string = mb_strtolower($lang === 'tr' ? str_replace('i', 'İ', $string) : $string, 'UTF-8');
+            break;
+        case 'ucwords':
+            $string = ltrim(mb_convert_case($lang === 'tr' ? str_replace(array( ' I', ' ı', ' İ', ' i' ), array( ' I', ' I', ' İ', ' İ' ), ' '.$string) : $string, MB_CASE_TITLE, 'UTF-8'));
+            break;
+        case 'ucfirst':
+            $first = mb_substr($string, 0, 1, 'UTF-8');
+            $string = mb_strtoupper($lang === 'tr' ? str_replace('i', 'İ', $first) : $first, 'UTF-8') . mb_substr($string, 1, mb_strlen($string, 'UTF-8') - 1, 'UTF-8');
+            break;
+    }
+
+    return $string;
 }
